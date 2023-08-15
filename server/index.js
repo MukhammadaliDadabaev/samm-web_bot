2f04c731d7ea08ae67cca8029fcfd607b95e7d70
Mukhammadali <ali_6017@mail.ru>
2023-08-15 21:58:09 +0500

    practice Telegram bot start

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
-console.log(2)
+const TelegramBot = require("node-telegram-bot-api")
+const token = "5732222616:AAHWAzjRVh3YoTOwF71LUElLrKrHD632SPo"
+
+const bot = new TelegramBot(token, {
+  polling: true
+})
+
+const webNodeBot = () => {
+  bot.on("message", async msg => {
+    const chatId = msg.chat.id
+    const text = msg.text
+
+    if (text === "/start") {
+      await bot.sendMessage(chatId, "Xush kelibsiz kurs sotib olish botiga...")
+    }
+  })
+}
+
+webNodeBot()
